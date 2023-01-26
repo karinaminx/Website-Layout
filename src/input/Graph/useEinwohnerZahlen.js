df68c7125b0c61a6bc7a8f4119ab45de619fbe1e
ulrke <ulrke@student.kit.edu>
2023-01-26 11:40:08 +0100

    pro 100.000 EW

--- src/input/Graph/useEinwohnerZahlen.js
+++ src/input/Graph/useEinwohnerZahlen.js
@@ -1,33 +1,27 @@
-// import React, { useState, useEffect } from "react";
-// import { csv } from "d3";
-// import "./Graph.css";
-
-// const csvUrl =
-// "https://raw.githubusercontent.com/KITmetricslab/hospitalization-nowcast-hub/main/other_data/population_sizes.csv";
-
-// export function useEinwohnerZahlen(menuAge, selectedScope) {
-
-  
-//   if(!menuAge){menuAge ="00+"}
-//   if(!selectedScope){selectedScope="DE"}
+ // Werte Einwohner Bundesländer / per Age Group
 
 
-//   const [data, setData] = useState(null);
+export const DE00 = 83138368;
+export const DEBW = 11097559;
+export const DEBY = 13143271;
+export const DEBE = 3663699;
+export const DEBB = 2522277;
+export const DEHB = 680058;
+export const DEHH = 1855315;
+export const DEHE = 6289594;
+export const DEMV = 1617005;
+export const DENI = 8020228;
+export const DENW = 17933411;
+export const DERP = 4115030;
+export const DESL = 985524;
+export const DESN = 4088171;
+export const DEST = 2179946;
+export const DESH = 2929662;
+export const DETH = 2120909;
+export const age00to04 = 3954455;
+export const age05to14 = 7504156;
+export const age15to34 = 18915114;
+export const age35to59 = 28676427;
+export const age60to79 = 18150355;
+export const age80up = 5934038;
 
-//   useEffect(() => {
-//     const row = (d) => {
-//       d.population = +d.population;
-
-//       return d;
-//     };
-
-//     csv(csvUrl, row).then((loadedData) => {
-//       const filteredData = loadedData.filter(
-//         (d) => d.location === selectedScope && d.age_group === menuAge
-//       );
-//       setData(filteredData);
-//     });
-//   }, [menuAge, selectedScope]);
-
-//   return data;
-// }
