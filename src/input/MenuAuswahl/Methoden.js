52856a053ac1e813d00c4acadc852e9ccc1c3df2
ulrke <ulrke@student.kit.edu>
2023-01-31 16:39:52 +0100

    hover button

--- src/input/MenuAuswahl/Methoden.js
+++ src/input/MenuAuswahl/Methoden.js
@@ -286,7 +286,7 @@ export const MethodenDiv = () => {
     <div>
       <div className=" buttonschart">
       
-      <span class="hovertext hoverm" data-hover={t("buttonleftchart")}>
+      <span class="hovertext hovermr" data-hover={t("buttonrightchart")}>
       <button className="btn-outline-dark buttonmovearea">
           
             <p>
@@ -294,7 +294,7 @@ export const MethodenDiv = () => {
             </p>
       </button>
       </span>
-      <span class="hovertext hoverm" data-hover={t("buttonrightchart")}>
+      <span class="hovertext hoverml" data-hover={t("buttonleftchart")}>
       <button className=" btn-outline-dark buttonmovearea">
             <p>
             <i className="fa-solid fa-caret-right"></i> 
